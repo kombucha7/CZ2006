ef19ba77b36712070e98523587c8e8e9590c9476
bluemaple78 <angraysean12@gmail.com>
2022-02-28 13:58:14 +0800

    navigation

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/MainActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.loginbutton)
+        button.setOnClickListener({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        })
     }
 }
