ac8f5d3449085ff29c3cfb1bda1475b0f67e9ab1
Paing Min Htet <philiplee98@gmail.com>
2022-02-28 13:07:14 +0800

    Update LoginActivity.kt
    -commented in Configure Google Sign In

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/LoginActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/LoginActivity.kt
@@ -2,10 +2,21 @@ package com.example.cz2006ver2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+    // Configure Google Sign In
+//    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//        .requestIdToken(getString(R.string.default_web_client_id))
+//        .requestEmail()
+//        .build()
+
+//    private lateinit var googleSignInClient: GoogleSignInClient
+//    googleSignInClient = GoogleSignIn.getClient(this, gso)
 }
