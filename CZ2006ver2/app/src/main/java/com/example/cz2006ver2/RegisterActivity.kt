d6cd8e2cc8384e16a1b3d3ca5fead4a658a19788
bluemaple78 <angraysean12@gmail.com>
2022-03-01 11:59:27 +0800

    added in creation of acc. testActivity dont use

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/RegisterActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/RegisterActivity.kt
@@ -1,11 +1,55 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_register2.*
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var emailEt: EditText
+    private lateinit var passwordEt: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register2)
+
+        registerbutton.setOnClickListener {auth = FirebaseAuth.getInstance()
+
+            emailEt = findViewById(R.id.emailfield)
+            passwordEt = findViewById(R.id.passwordfield1)
+
+
+            registerbutton.setOnClickListener{
+                var email: String = emailEt.text.toString()
+                var password: String = passwordEt.text.toString()
+
+                if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_LONG).show()
+                } else{
+                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener{ task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
+                            val intent = Intent(this, testActivity1::class.java)
+                            startActivity(intent)
+                            finish()
+                        }else {
+                            Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
+                        }
+                    })
+                }
+            }
+        }
+
     }
 }
