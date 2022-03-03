0039ac56fd2c0bfd22fbb1a4fa0f8a1d8a1106bc
bluemaple78 <angraysean12@gmail.com>
2022-03-04 00:41:11 +0800

    backend shown on groupselectionpage

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupselectionActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupselectionActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.toObject
@@ -11,22 +12,13 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 class GroupselectionActivity : AppCompatActivity() {
 
-    data class careRecipientInfo(        //data that we are passing in, add on to userInfo if want to ask for more
-        val cr1: String? = null,
-        val cr2: String? = null,
-        val cr3: String? = null,
-        val cr4: String? = null,
-        val cr5: String? = null
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_groupselection)
 
-        val careRecipient_Info : careRecipientInfo
 
         getFirestore()
-
         //back button to the landing page
         back_button_word.setOnClickListener{
             val groupSelectBackBtn = Intent(this, LoginActivity::class.java)
@@ -36,6 +28,8 @@ class GroupselectionActivity : AppCompatActivity() {
 
 
     fun getFirestore(){
+        val cr1 = findViewById<TextView>(R.id.gs_group1)
+        val cr2 = findViewById<TextView>(R.id.gs_group2)
         val currentFirebaseUser = FirebaseAuth.getInstance().currentUser    //getting the user id value
         val userID = currentFirebaseUser!!.uid
         val TAG = "myLogTag"
@@ -44,7 +38,9 @@ class GroupselectionActivity : AppCompatActivity() {
         docRef.get()
             .addOnSuccessListener { document ->
                 if (document != null) {
-                 Log.d(TAG, "Our data: ${document.data}")
+                 //Log.d(TAG, "Our data: ${document.data}")
+                    cr1.text = document.getString("careRecipient1")
+                    cr2.text = document.getString("careRecipient2")
                 }
             }
             .addOnFailureListener { exception ->
