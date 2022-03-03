cf0ce1485c2d7ab91d0ddfb880e2de69989f4468
cel-ong <celestineoxr@gmail.com>
2022-03-03 22:53:20 +0800

    changed calendar activity name

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/CalendarMainActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/CalendarMainActivity.kt
@@ -3,7 +3,7 @@ package com.example.cz2006ver2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class calendar_main : AppCompatActivity() {
+class CalendarMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_main)
