b6a30cd4178b3aac5c7c121a682973f86ae61848
cel-ong <celestineoxr@gmail.com>
2022-03-04 01:34:51 +0800

    view profile back btn logic

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/ViewProfileActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/ViewProfileActivity.kt
@@ -1,11 +1,19 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class ViewProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_profile)
+
+        //back button to the landing page
+        back_button_word.setOnClickListener{
+            val viewProfileBackBtn = Intent(this, AccountMainActivity::class.java)
+            startActivity(viewProfileBackBtn)
+        }
     }
 }
