488b1b4f967aeb7cfec0b3587a99d1c5f4dda0bf
bluemaple78 <angraysean12@gmail.com>
2022-03-03 21:28:38 +0800

    firestore read n write not completed for groupselect page

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupselectionActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupselectionActivity.kt
@@ -3,12 +3,29 @@ package com.example.cz2006ver2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 import kotlinx.android.synthetic.main.activity_login.*
 
 class GroupselectionActivity : AppCompatActivity() {
+
+    data class careRecipientInfo(        //data that we are passing in, add on to userInfo if want to ask for more
+        val cr1: String? = null,
+        val cr2: String? = null,
+        val cr3: String? = null,
+        val cr4: String? = null,
+        val cr5: String? = null
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_groupselection)
+
+        val careRecipient_Info : careRecipientInfo
+
+        getFirestore()
 
         //back button to the landing page
         back_button_word.setOnClickListener{
@@ -16,4 +33,23 @@ class GroupselectionActivity : AppCompatActivity() {
             startActivity(groupSelectBackBtn)
         }
     }
+
+
+    fun getFirestore(){
+        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser    //getting the user id value
+        val userID = currentFirebaseUser!!.uid
+        val TAG = "myLogTag"
+        val db = FirebaseFirestore.getInstance()
+        val docRef = db.collection("careRecipient").document("testID")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                 Log.d(TAG, "Our data: ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
+    }
+
 }
--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/testActivity1.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/testActivity1.kt
@@ -1,16 +1,25 @@
 package com.example.cz2006ver2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_test1.*
 
+
 class testActivity1 : AppCompatActivity() {
+
 
     private lateinit var testUserName: EditText
     private lateinit var testUserPhoneNum: EditText
+
+    data class userInfo(        //data that we are passing in, add on to userInfo if want to ask for more
+        val name: String? = null,
+        val num: String? = null,
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,25 +29,47 @@ class testActivity1 : AppCompatActivity() {
             testUserName = findViewById(R.id.testEnterName)     //this is an edittext object?? need to convert to string first
             testUserPhoneNum = findViewById(R.id.testEnterPhoneNum)
 
-            val name = testUserName.text.toString()
+            val name = testUserName.text.toString()     //convert to string at this stage
             val num = testUserPhoneNum.text.toString()
             saveFireStore(name,num)
+            getFireStore()
         }
     }
 
-    fun saveFireStore(name: String, num: String) {
+    fun saveFireStore(name: String, num: String) {      //function for posting stuff
+        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
+        val userID = currentFirebaseUser!!.uid
+        //Toast.makeText(this, "" + currentFirebaseUser!!.uid, Toast.LENGTH_SHORT).show()   just for testing
         val db = FirebaseFirestore.getInstance()
-        val user: MutableMap<String, Any> = HashMap()
-        user["name"] = name
-        user["number"] = num
 
-        db.collection("users")
-            .add(user)
+        val data = userInfo(name,num)
+
+        db.collection("users").document(userID).set(data)
             .addOnSuccessListener {
                 Toast.makeText(this@testActivity1, "record added successfully ", Toast.LENGTH_SHORT ).show()
             }
+
             .addOnFailureListener{
                 Toast.makeText(this@testActivity1, "record Failed to add ", Toast.LENGTH_SHORT ).show()
+            }
+    }
+
+    fun getFireStore() {        //function for getting stuff
+        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser    //getting the user id value
+        val userID = currentFirebaseUser!!.uid
+        val TAG = "myLogTag"
+        val db = FirebaseFirestore.getInstance()
+        val docRef = db.collection("users").document(userID)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "Our data: ${document.data}")
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
             }
     }
 
