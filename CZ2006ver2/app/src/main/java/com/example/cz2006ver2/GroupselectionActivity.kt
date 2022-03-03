abf59df0610a9337f0cc27e192b30ae0e6912eaa
cel-ong <celestineoxr@gmail.com>
2022-03-03 18:30:25 +0800

    added padding and logic

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupselectionActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupselectionActivity.kt
@@ -1,11 +1,19 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class GroupselectionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_groupselection)
+
+        //back button to the landing page
+        back_button_word.setOnClickListener{
+            val groupSelectBackBtn = Intent(this, LoginActivity::class.java)
+            startActivity(groupSelectBackBtn)
+        }
     }
 }
