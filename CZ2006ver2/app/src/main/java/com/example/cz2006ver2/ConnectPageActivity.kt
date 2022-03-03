e64c13a998d0369b61499b6f0fdc3411e610ffe5
bluemaple78 <angraysean12@gmail.com>
2022-03-03 17:39:50 +0800

    renamed files

---
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/ConnectPageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cz2006ver2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConnectPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect_page)
+    }
+}
---
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupNamePageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cz2006ver2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroupNamePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_group_name_page)
+    }
+}
