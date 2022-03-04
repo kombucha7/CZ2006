f9ea08f39df8932be689366fb9ae34e967ceab90
kombucha7 <chanhuide@gmail.com>
2022-03-04 15:05:07 +0800

    Added most homepage1 functionality

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/HomePage1.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/HomePage1.kt
@@ -1,11 +1,40 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_home_page1.*
 
 class HomePage1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page1)
+    }
+
+    fun HomeClick_page1(view: View) {
+        homeicon_page1.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun CalendarClick_page1(view: View) {
+        calendaricon_page1.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun TransportClick_page1(view: View) {
+        transporticon_page1.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    fun AccountClick_page1(view: View) {
+        accounticon_page1.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
