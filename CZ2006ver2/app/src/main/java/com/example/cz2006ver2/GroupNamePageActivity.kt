817eb7319acd75fd4953f5838fe24fb7a2c688eb
bluemaple78 <angraysean12@gmail.com>
2022-03-08 20:39:57 +0800

    stored elderUID into user

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupNamePageActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/GroupNamePageActivity.kt
@@ -1,10 +1,12 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_group_name_page.*
 import kotlinx.android.synthetic.main.activity_test1.*
@@ -28,15 +30,18 @@ class GroupNamePageActivity : AppCompatActivity() {
             elderName = findViewById(R.id.groupNameField)
             val elderNameString = elderName.text.toString()     //convert to string at this stage
             if (elderUID != null) {
-                saveFireStore(elderNameString,elderUID)
+                saveElderInfo(elderNameString,elderUID)
+                addElderToUser(elderUID)
             }
+            val intent = Intent(this, HomePage1::class.java)
+            startActivity(intent)
         }
     }
 
 
 
 
-    fun saveFireStore(elderName: String,elderKey : String) {      //function for posting stuff
+    fun saveElderInfo(elderName: String,elderKey : String) {      //function for posting stuff
         val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
         val userID = currentFirebaseUser!!.uid
         //Toast.makeText(this, "" + currentFirebaseUser!!.uid, Toast.LENGTH_SHORT).show()   just for testing
@@ -55,6 +60,23 @@ class GroupNamePageActivity : AppCompatActivity() {
     }
 
 
+    fun addElderToUser(elderUID : String) {      //function for posting stuff
+        //get instance of user
+        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
+        val userID = currentFirebaseUser!!.uid
+        val db = FirebaseFirestore.getInstance()
 
+        val data = elderInfo(elderUID) //check on top to find data that we add in. need to add more eventually
+        val userCareRecipientArray = db.collection("users").document(userID)
+
+        userCareRecipientArray.update("careArray", FieldValue.arrayUnion(elderUID))
+            .addOnSuccessListener {
+                Toast.makeText(this@GroupNamePageActivity, "record added successfully ", Toast.LENGTH_SHORT ).show()
+            }
+
+            .addOnFailureListener{
+                Toast.makeText(this@GroupNamePageActivity, "record Failed to add ", Toast.LENGTH_SHORT ).show()
+            }
+    }
 
 }
