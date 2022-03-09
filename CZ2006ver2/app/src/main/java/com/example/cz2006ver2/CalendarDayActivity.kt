3447e74669b474c516ada50d3ec8f50d2ade23ee
cel-ong <celestineoxr@gmail.com>
2022-03-09 17:06:49 +0800

    calendar edits

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/CalendarDayActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/CalendarDayActivity.kt
@@ -1,11 +1,21 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_home_page1.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class CalendarDayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_day)
+
+        //back button to the calendar main page
+        back_button_word.setOnClickListener{
+            val calDayBackBtn = Intent(this, CalendarMainActivity::class.java)
+            startActivity(calDayBackBtn)
+        }
     }
 }
--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/CalendarMainActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/CalendarMainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_calendar_main.*
+import kotlinx.android.synthetic.main.activity_home_page1.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class CalendarMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar_main)
+
+        //nav bar
+        homeicon_page2.setOnClickListener{
+            val intent = Intent(this, HomePage1::class.java)
+            startActivity(intent)
+        }
+        calendaricon_page2.setOnClickListener{
+            val intent = Intent(this, CalendarMainActivity::class.java)
+            startActivity(intent)
+        }
+        transporticon_page2.setOnClickListener{
+            val intent = Intent(this, CalendarDayActivity::class.java)
+            startActivity(intent)
+        }
+        accounticon_page2.setOnClickListener{
+            val intent = Intent(this, AccountMainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
