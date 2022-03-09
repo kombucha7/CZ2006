81f3367866c77f34703b01044c17011302c174ad
cel-ong <celestineoxr@gmail.com>
2022-03-09 20:18:39 +0800

    new calendar page

---
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/CalendarCaretakerActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cz2006ver2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_calendar_caretaker.*
+import kotlinx.android.synthetic.main.activity_calendar_day.*
+
+class CalendarCaretakerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar_caretaker)
+
+        //back button to the calendar day page
+        back_word_cal2.setOnClickListener{
+            val cal2BackBtn = Intent(this, CalendarDayActivity::class.java)
+            startActivity(cal2BackBtn)
+        }
+    }
+}
