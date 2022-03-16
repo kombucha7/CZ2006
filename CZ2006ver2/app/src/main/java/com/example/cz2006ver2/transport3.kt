2b2f421ec52e8e204bb72a6fe857e670820a1548
GodForsake <dhruv010@e.ntu.edu.sg>
2022-03-16 11:54:15 +0800

    h

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/transport3.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/transport3.kt
@@ -1,11 +1,21 @@
 package com.example.cz2006ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_transport3.*
 
 class transport3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transport3)
+        back_btn_cal2.setOnClickListener {
+            val back1 = Intent(this, trans1::class.java)
+            startActivity(back1)
+        }
+        back_word_cal2.setOnClickListener {
+            val back2 = Intent(this, trans1::class.java)
+            startActivity(back2)
+        }
     }
 }
