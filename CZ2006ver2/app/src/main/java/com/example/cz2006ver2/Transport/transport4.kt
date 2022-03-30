b3d9ad9e4323c2912b4aba7971d4200f4d89a9f8
bluemaple78 <angraysean12@gmail.com>
2022-03-31 00:40:47 +0800

    trans4pull bus stop code list

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/Transport/transport4.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/Transport/transport4.kt
@@ -1,5 +1,6 @@
 package com.example.cz2006ver2.Transport
 
+import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,9 +8,15 @@ import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
+import com.example.cz2006ver2.HomePage.HomePage1
+import com.example.cz2006ver2.HomePage.Todo
 import com.example.cz2006ver2.R
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
+import kotlinx.android.synthetic.main.activity_test_transport_api.*
 import kotlinx.android.synthetic.main.activity_transport2.*
 
 /**
@@ -20,24 +27,26 @@ class transport4 : AppCompatActivity() {
      * Method used to start default activity. Link back to main Transport Page.
      * @param savedInstanceState to get prior version. If no data is supplies, then NULL.
      */
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {        /**we need to pass the elderUID into this page as intent**/
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transport4)
-        val list: MutableList<String> = ArrayList()
-        val busStopCode:String = intent.getStringExtra("BusStopCode").toString()
-        if (busStopCode != null) {
-            list.add(busStopCode)
-        }
+        val elderUID : String = "un5zqQK0"
+//        val list: MutableList<String> = ArrayList()
+//        val busStopCode:String = intent.getStringExtra("BusStopCode").toString()
+//        if (busStopCode != null) {
+//            list.add(busStopCode)
+//        }
         //this dropdown is for the bus numbers on that particular busstop
         //if dropdown is to show the available bus stop codes instead, then need another for loop
 //        for (i in 0 until services.length()) {
 //            val busNum = services.getJSONObject(i).getString("ServiceNo")
 //            list.add(busNum)
 //        }
-        var busStopListView: ListView = findViewById(R.id.busStopList)
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
-        busStopListView.adapter = arrayAdapter
-
+//
+//        var busStopListView: ListView = findViewById(R.id.busStopList)
+//        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+//        busStopListView.adapter = arrayAdapter
+        getFavouriteBusStopCode(elderUID)
         back_btn_cal2.setOnClickListener {
             val back1 = Intent(this, trans1::class.java)
             startActivity(back1)
@@ -48,35 +57,29 @@ class transport4 : AppCompatActivity() {
         }
     }
 
-    private fun getFavouriteBusStop(elderKey : String, busStopCode: String){
-
+    private fun getFavouriteBusStopCode(elderUID : String) {
+        var busStopCodeList: MutableList<String> = ArrayList()
         val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
         val userID = currentFirebaseUser!!.uid
         //Toast.makeText(this, "" + currentFirebaseUser!!.uid, Toast.LENGTH_SHORT).show()   just for testing
         val db = FirebaseFirestore.getInstance()
 
-        val collectionRef = db.collection("careRecipient").document("un5zqQK0").collection("favBusStop")
-        collectionRef.get()
-            .addOnSuccessListener { collection ->
-                if (collection != null) {
-                    Log.d("222", "document found")
-//                    collection.id
+        db.collection("careRecipient").document(elderUID).collection("favBusStop").get()
+            .addOnCompleteListener(OnCompleteListener<QuerySnapshot?> { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result) {
+                        busStopCodeList.add(document.id)    /**adding each document to our list as a string**/
+                    }
+                    Log.d(ContentValues.TAG, "the tasks we have " + busStopCodeList.toString())
+                    for (i in busStopCodeList){/** for testing**/
+                        println("Here " + i)
+                    }
+                    /**start coding here**/
+
+
                 } else {
-                    Log.d("222", "No such document")
+                    Log.d(ContentValues.TAG, "Error getting documents: ", task.exception)
                 }
-            }
-            .addOnFailureListener { exception ->
-                Log.d("222", "get failed with ", exception)
-            }
-
-//        db.collection("careRecipient").document(elderKey).collection("favBusStop").document(busStopCode).set(setFav)
-//            .addOnSuccessListener {
-//                Toast.makeText(this@transport3, "record added successfully ", Toast.LENGTH_SHORT ).show()
-//            }
-//
-//            .addOnFailureListener{
-//                Toast.makeText(this@transport3, "record Failed to add ", Toast.LENGTH_SHORT ).show()
-//            }
-
+            })
     }
 }
