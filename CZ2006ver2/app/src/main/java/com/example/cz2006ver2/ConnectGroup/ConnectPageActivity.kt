f4459ea0f9fa621ed086e2bfb0d90614fa773bb4
Paing Min Htet <philiplee98@gmail.com>
2022-04-02 17:28:13 +0800

    connectpage

--- CZ2006ver2/app/src/main/java/com/example/cz2006ver2/ConnectGroup/ConnectPageActivity.kt
+++ CZ2006ver2/app/src/main/java/com/example/cz2006ver2/ConnectGroup/ConnectPageActivity.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
 import com.example.cz2006ver2.HomePage.HomePage1
@@ -45,30 +46,34 @@ class ConnectPageActivity : AppCompatActivity() {
 
         connectEnterButton.setOnClickListener{
             val groupCode = connectGroupCodeField.text
-            enterGroupCode(groupCode.toString()) //in this func, we direct to same page if unsuccessful. look at func below
+            if (TextUtils.isEmpty(groupCode)){
+                Toast.makeText(this@ConnectPageActivity, "Field cannot be empty.", Toast.LENGTH_SHORT ).show()
+            }else {
+                enterGroupCode(groupCode.toString()) //in this func, we direct to same page if unsuccessful. look at func below
 
 
-            addElderToUser(groupCode.toString())//adding the elderly to the user's array i dont know if this function is needed i think i wrote an extra one
+//            addElderToUser(groupCode.toString())//adding the elderly to the user's array i dont know if this function is needed i think i wrote an extra one
 
-            ///////////////////async function so we cant implement as normal func///////////////////////////////////////
-            val docRef = db.collection("users").document(userID)
-            docRef.get()
-                .addOnSuccessListener { document ->
-                    if (document != null) {
-                        println("persons name " + document.get("name").toString())
-                        var personname = document.get("name".toString()).toString()
-                        addUserToElderSubCol(groupCode.toString(),personname)   //saving the caretaker data to elderly subcollection
-                    } else {
-                        Log.d(ContentValues.TAG, "No such document")
+                ///////////////////async function so we cant implement as normal func///////////////////////////////////////
+                val docRef = db.collection("users").document(userID)
+                docRef.get()
+                    .addOnSuccessListener { document ->
+                        if (document != null) {
+                            println("persons name " + document.get("name").toString())
+                            var personName = document.get("name").toString()
+                            addUserToElderSubCol(groupCode.toString(), personName)   //saving the caretaker data to elderly subcollection
+                        } else {
+                            Log.d(ContentValues.TAG, "No such document")
+                        }
                     }
-                }
-                .addOnFailureListener { exception ->
-                    Log.d(ContentValues.TAG, "get failed with ", exception)
-                }
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            val intent = Intent(this, HomePage1::class.java)    //need to also implement the function to add this user to the elderly caretaker list
-            intent.putExtra("key", groupCode.toString())
-            startActivity(intent)
+                    .addOnFailureListener { exception ->
+                        Log.d(ContentValues.TAG, "get failed with ", exception)
+                    }
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                val intent = Intent(this,HomePage1::class.java)    //need to also implement the function to add this user to the elderly caretaker list
+                intent.putExtra("key", groupCode.toString())
+                startActivity(intent)
+            }
         }
     }
 
@@ -96,6 +101,7 @@ class ConnectPageActivity : AppCompatActivity() {
                     addElderKeyToUser(groupCode)
                 } else {
                     Log.d(TAG, "No such document")                                      //if unsuccessful, just start intent at this page again
+                    Toast.makeText(this@ConnectPageActivity, "Group does not exist ", Toast.LENGTH_SHORT ).show()
                     val intent = Intent(this, ConnectPageActivity::class.java)
                     startActivity(intent)
                 }
@@ -134,7 +140,7 @@ class ConnectPageActivity : AppCompatActivity() {
 
         db.collection("careRecipient").document(elderUID).collection("caretaker").document(userID).set(data)
             .addOnSuccessListener {
-                Toast.makeText(this@ConnectPageActivity, "record added successfully ", Toast.LENGTH_SHORT ).show()
+//                Toast.makeText(this@ConnectPageActivity, "record added successfully ", Toast.LENGTH_SHORT ).show()
             }
 
             .addOnFailureListener{
@@ -142,23 +148,23 @@ class ConnectPageActivity : AppCompatActivity() {
             }
     }
 
-    fun addElderToUser(elderUID : String) {      //function for posting stuff
-        //get instance of user
-        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
-        val userID = currentFirebaseUser!!.uid
-        val db = FirebaseFirestore.getInstance()
-
-        val data =
-            GroupNamePageActivity.elderInfo(elderUID) //check on top to find data that we add in. need to add more eventually
-        val userCareRecipientArray = db.collection("users").document(userID)
-
-        userCareRecipientArray.update("careArray", FieldValue.arrayUnion(elderUID))
-            .addOnSuccessListener {
-                Toast.makeText(this@ConnectPageActivity, "record added successfully ", Toast.LENGTH_SHORT ).show()
-            }
-
-            .addOnFailureListener{
-                Toast.makeText(this@ConnectPageActivity, "record Failed to add ", Toast.LENGTH_SHORT ).show()
-            }
-    }
+//    fun addElderToUser(elderUID : String) {      //function for posting stuff
+//        //get instance of user
+//        val currentFirebaseUser = FirebaseAuth.getInstance().currentUser
+//        val userID = currentFirebaseUser!!.uid
+//        val db = FirebaseFirestore.getInstance()
+//
+//        val data =
+//            GroupNamePageActivity.elderInfo(elderUID) //check on top to find data that we add in. need to add more eventually
+//        val userCareRecipientArray = db.collection("users").document(userID)
+//
+//        userCareRecipientArray.update("careArray", FieldValue.arrayUnion(elderUID))
+//            .addOnSuccessListener {
+////                Toast.makeText(this@ConnectPageActivity, "record added successfully ", Toast.LENGTH_SHORT ).show()
+//            }
+//
+//            .addOnFailureListener{
+//                Toast.makeText(this@ConnectPageActivity, "record Failed to add ", Toast.LENGTH_SHORT ).show()
+//            }
+//    }
 }
